c141c1efa3e277e29c3b8d732aaf6e57fe9bee45
GarvielL0ken <garviel.loken.42@gmail.com>
2021-12-05 17:26:50 +0200

    site/js/classes/*.js: Added the classes responsible for the handling of fetch requests

---
+++ site/js/classes/RequestData.js
@@ -0,0 +1,6 @@
+export class RequestData {
+	constructor(name, path) {
+		this.name = name;
+		this.path = "../config/fetch_points/" + path;
+	}
+}
---
+++ site/js/classes/RequestHandler.js
@@ -0,0 +1,71 @@
+import { RequestData } from './RequestData.js';
+export class RequestHandler {
+	constructor(requestData= null) {
+		/** @private {!responseText}*/
+		this.responseText_ = null;
+
+		/** @private {!arrRequestData} */
+		this.arrRequestData_ = [];
+		this.initializeRequestData(requestData);
+	}
+
+	initializeRequestData(requestData) {
+		var	i;
+
+		i = 0;
+		while(requestData[i]) {
+			this.addNewRequestData(requestData[i][0], requestData[i][1]);
+			i++;
+		}
+	}
+
+	/*	____ACCESSOR METHODS____	*/
+	/*		____GET____				*/
+
+	//Used to find a requestData object by name
+	getRequestN(requestName) {
+		var i;
+
+		i = 0;
+		while(this.arrRequestData_[i]) {
+			if (this.arrRequestData_[i].name === requestName)
+				return(this.arrRequestData_[i]);
+			i++;
+		}
+
+		return(false);
+	}
+
+	/*		____SET____				*/
+	addNewRequestData(name, path) {
+		var newRequestData = new RequestData(name, path);
+
+		this.arrRequestData_.push(newRequestData);
+	}
+
+	setResponseText(responseText) {
+		this.responseText_ = responseText;
+	}
+
+	/*	____STANDARD METHODS____		*/
+	/*		____REQUEST METHODS___	*/
+	//Send Request using the data provided in the requestData object
+	//Returns the response text
+	async fetch(requestData) {
+		var	response	=null;
+		var	data		=null;
+		
+		response = await fetch(requestData.path);
+		data = await response.text();
+		
+		return (data);
+	}
+
+	async fetchN(requestName, setToCurrent=false) {
+		var request;
+
+		request = this.getRequestN(requestName);
+
+		return(this.fetch(request));
+	}
+}
