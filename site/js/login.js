79ed662ea876f310857b2d55fb06864839399f57
GarvielL0ken <garviel.loken.42@gmail.com>
2021-12-05 17:23:58 +0200

    ./site/js/login.js->application{}: added errorMessage Property, Added getUrlParameter and sendRequest methods, Added mounted(); getURLParameter gets the value of a parameter that is passed. sendRequest will call RequstHandler fetch method and provide a callback function, mounted() changes the page varible to that specified in the URL, or sets it to a default value; and changes the errorMessage to that specified in the URL or sets it to null

--- site/js/login.js
+++ site/js/login.js
@@ -1,11 +1,45 @@
 var application = new Vue({
 	el : "#application",
 	data : {
-		page : "sign-in"
+		page : "sign-in",
+		errorMessage : ""
+		//RequsetHandler : new RequestHandler();
 	},
 	methods : {
 		changePage(page) {
 			this.page = page;
-		}
+		},
+		/*Gets the value in a given parameter in the current url*/
+		/*Retruns Null if no paramter is found in the url*/
+		getURLParameter(parameter) {
+			var	url;
+			var	regex;
+			var	value;
+
+			url = window.location.href;
+			regex = parameter + '=([^&]+)';
+			regex = new RegExp(regex);
+			
+			/*returns: 
+				[0: parameter=value, 1: value] hence
+				value[1]
+			*/
+			value = regex.exec(url);
+			if (value)
+				return (value[1]);
+			return (null);
+		},
+		sendRequest(requestName= "", callback) {
+			this.RequestHandler.fetch(requestName).then((response) => callback(response));
+		},
+	},
+	mounted() {
+		var	result;
+
+		result = this.getURLParameter("page");
+		this.page = result ? result : "sign-in";
+
+		this.errorMessage = this.getURLParameter("error-message");
+		this.errorMessage = decodeURI(this.errorMessage);
 	}
 })
