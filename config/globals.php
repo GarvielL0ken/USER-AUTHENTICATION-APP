04868438e71fe88b25ba79a55728a13c88a5cecc
GarvielL0ken <garviel.loken.42@gmail.com>
2021-12-12 09:33:21 +0200

    Corrected the spelling of RGX_PASSWD; Add RGX_PATTERNS and TABLE_FIELD_PAIRS

--- config/globals.php
+++ config/globals.php
@@ -1,5 +1,12 @@
 <?php
 	$RGX_EMAIL = '^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+$';
-	$RGX_PASSWROD = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])(?=.{8,32})[0-9]+$';
+	$RGX_PASSWD = '^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])(?=.*[0-9])(?=.{8,32})$';
 	$RGX_USERNAME = '^(?=.{5,32}$)[a-zA-Z0-9._]+$';
+	$RGX_PATTERNS = ['alpha' => '^[a-zA-Z ]+$',
+						'numeric' => '^[\d]+$',
+						'genres' => '^[a-zA-Z, ]+$'];
+	$TABLE_FIELD_PAIRS = ['authors' => 'name, age, genres',
+							'books' => 'title',
+							'genres' => 'genre',
+							'users' => 'username, role'];
 ?>
