57e65e32269b5459facc9f1fbe7fd695cb2e1f28
GarvielL0ken <garviel.loken.42@gmail.com>
2021-12-12 10:00:40 +0200

    moved getURLParameter to lig.js

--- site/js/login.js
+++ site/js/login.js
@@ -1,3 +1,5 @@
+import { getURLParameter } from './lib.js';
+
 var application = new Vue({
 	el : "#application",
 	data : {
@@ -8,38 +10,15 @@ var application = new Vue({
 	methods : {
 		changePage(page) {
 			this.page = page;
-		},
-		/*Gets the value in a given parameter in the current url*/
-		/*Retruns Null if no paramter is found in the url*/
-		getURLParameter(parameter) {
-			var	url;
-			var	regex;
-			var	value;
-
-			url = window.location.href;
-			regex = parameter + '=([^&]+)';
-			regex = new RegExp(regex);
-			
-			/*returns: 
-				[0: parameter=value, 1: value] hence
-				value[1]
-			*/
-			value = regex.exec(url);
-			if (value)
-				return (value[1]);
-			return ('');
-		},
-		sendRequest(requestName= "", callback) {
-			this.RequestHandler.fetch(requestName).then((response) => callback(response));
-		},
+		}
 	},
 	mounted() {
 		var	result;
 
-		result = this.getURLParameter("page");
+		result = getURLParameter(window.location.href, "page");
 		this.page = result ? result : "sign-in";
 
-		this.errorMessage = this.getURLParameter("error-message");
+		this.errorMessage = getURLParameter("error-message");
 		if (this.errorMessage)
 			this.errorMessage = decodeURI(this.errorMessage);
 	}
